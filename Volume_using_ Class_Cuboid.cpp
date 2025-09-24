85bda02176fb6d35524fe32aec9402bf14551a02
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 12:07:30 +0530

    Create Volume_using_ Class_Cuboid.cpp

---
+++ Volume_using_ Class_Cuboid.cpp
@@ -0,0 +1,24 @@
+//Anjaneya Kalchuri
+//24070123015
+//Batch: A1
+
+#include <iostream>
+using namespace std;
+class cuboid {
+    public:
+    int height = 3;
+    int width = 2;
+    int length = 4;
+};
+
+int main() {
+    cuboid c1;
+    int volume = c1.height * c1.width * c1.length;
+    cout<<"Volume of the cuboid:"<<volume;
+    
+    return 0;
+}
+
+# OUTPUT:
+
+Volume of the cuboid:24
