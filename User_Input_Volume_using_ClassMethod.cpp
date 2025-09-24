00c525d013cd03bcd695e440197dda54dcbc0f0a
Andythej420 <dearanjaneya@gmail.com>
2025-09-24 12:06:54 +0530

    Create User_Input_Volume_using_ClassMethod.cpp

---
+++ User_Input_Volume_using_ClassMethod.cpp
@@ -0,0 +1,49 @@
+//Anjaneya Kalchuri
+//24070123015
+//Batch: A1
+
+#include <iostream>
+using namespace std;
+
+class cuboid {
+    public:
+    int height;
+    int width;
+    int length;
+
+    int volume() {
+    int v;
+    v = height * width * length;
+    
+    return v;
+    }
+    
+    void input(){
+        cout<<"Enter Height: ";
+        cin>>height;
+        cout<<"Enter width: ";
+        cin>>width;
+        cout<<"Enter Length: ";
+        cin>>length;
+    }
+    
+    void display(){
+        cout<<"\n"<<"Volume of the cuboid: "<<volume();
+    }
+};
+
+int main(){
+    cuboid cuboid1;
+
+    cuboid1.input();
+    
+    cuboid1.display();
+}
+
+# Output:
+
+Enter Height: 3
+Enter width: 2
+Enter Length: 1
+
+Volume of the cuboid: 6
